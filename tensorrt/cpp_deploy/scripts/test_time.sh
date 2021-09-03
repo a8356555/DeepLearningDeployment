097764e80af370851628e384b7963a8cfcd18208
a8356555 <a8356555@gmail.com>
2021-09-03 07:23:04 +0000

    modifying tensorRT

--- tensorrt/cpp_deploy/scripts/test_time.sh
+++ tensorrt/cpp_deploy/scripts/test_time.sh
@@ -2,13 +2,18 @@
 cd /content/DeepLearningDeployment/tensorrt/serialize_engine_from_onnx_cpp/bin
 for MODEL in resnet101 resnet50 efficientnet-b4 efficientnet-b5 efficientnet-b6 efficientnet-b7
 do
-    echo $MODEL
-    ./trt_serialize /content/$MODEL.onnx /content/$MODEL.trt
+    if [ -e /content/ONNX_MODELS/$MODEL.trt ]
+    then
+        echo "$MODEL.trt exists"
+    else
+        echo $MODEL
+        ./trt_serialize /content/ONNX_MODELS/$MODEL.onnx /content/ONNX_MODELS/$MODEL.trt
+    fi
 done
 
 cd /content/DeepLearningDeployment/tensorrt/cpp_deploy/bin
 for MODEL in resnet101 resnet50 efficientnet-b4 efficientnet-b5 efficientnet-b6 efficientnet-b7
 do
     echo $MODEL
-    ./trt_inference /content/$MODEL.trt /content/YuShanAICompetition/sample.jpg $1
+    ./trt_inference /content/ONNX_MODELS/$MODEL.trt /content/DeepLearningDeployment/sample.jpg $1
 done
