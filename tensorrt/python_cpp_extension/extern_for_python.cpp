6e2a100966aea94325a3eece987f986c1476a822
Alan <59360204+a8356555@users.noreply.github.com>
2021-09-09 20:05:47 +0800

    Update extern_for_python.cpp

--- tensorrt/python_cpp_extension/extern_for_python.cpp
+++ tensorrt/python_cpp_extension/extern_for_python.cpp
@@ -1,11 +1,16 @@
+// include folder for tensorRT: 
+// include folder for other folder src: /content/DeepLearningDeployment/tensorrt/cpp_deploy/src
+// include folder for Opencv 
 
+// lib for trt
+// lib for Opencv
 
-extern "C" {
-	char* predict(char *engine_path, char *image_path){
-		/** 
-	 	* 实现部分
-	 	**/
-		// 返回包含预测结果的json字符串
-		return result;
-	}
+// g++ -shared -std=c++11 -Wall -fPIC -o libtrtpy.so /content/DeepLearningDeployment/tensorrt/cpp_deploy/src/inference.cpp extern_for_python.cpp -I ??? -I /content/DeepLearningDeployment/tensorrt/cpp_deploy/src -L??? -lonnxruntime -Wl,-R,/home/luhsuanwen/onnxruntime/build/Linux/RelWithDebInfo/ -I /usr/local/include/opencv4/ -L/usr/local/lib -lopencv_calib3d -lopencv_core -lopencv_dnn -lopencv_features2d -lopencv_flann -lopencv_gapi -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -Wl,-R,/usr/local/lib
+// -lopencv_calib3d -lopencv_core -lopencv_dnn -lopencv_features2d -lopencv_flann -lopencv_gapi -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_stitching -lopencv_video -lopencv_videoio -lopencv_aruco -lopencv_barcode -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_datasets -lopencv_dnn_objdetect -lopencv_dnn_superres -lopencv_dpm -lopencv_face -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_intensity_transform -lopencv_line_descriptor -lopencv_mcc -lopencv_optflow -lopencv_phase_unwrapping -lopencv_plot -lopencv_quality -lopencv_rapid -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_shape -lopencv_stereo -lopencv_structured_light -lopencv_superres -lopencv_surface_matching -lopencv_text -lopencv_tracking -lopencv_videostab -lopencv_wechat_qrcode -lopencv_xfeatures2d -lopencv_ximgproc -lopencv_xobjdetect -lopencv_xphoto
+
+#include "inference.h"
+extern "C" float* trt_inference(char* modelPath, char* imagePath){
+	float* output = new float[801];
+	predict(modelPath, imagePath, &output);
+	return output;
 }
