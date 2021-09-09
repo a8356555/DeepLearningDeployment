1dcef4fbd0cdef0e36ee76989b14ac0af43d6bd7
Alan <59360204+a8356555@users.noreply.github.com>
2021-09-10 01:09:28 +0800

    Update test_time.sh

--- tensorrt/cpp_deploy/scripts/test_time.sh
+++ tensorrt/cpp_deploy/scripts/test_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /content/DeepLearningDeployment/tensorrt/serialize_engine_from_onnx_cpp/bin
-for MODEL in resnet101 resnet50 efficientnet-b4 efficientnet-b5 efficientnet-b6 efficientnet-b7
+for MODEL in resnet50 resnet101 efficientnet-b4 efficientnet-b5 efficientnet-b6 efficientnet-b7
 do
     if [ -e /content/ONNX_MODELS/$MODEL.trt ]
     then
